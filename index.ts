b86aba7f96cf1f78fe453b10f8521910cf697e7e
irfanmrz <162377727+irfanmrz@users.noreply.github.com>
2024-04-19 17:32:45 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,44 @@
+import inquirer from "inquirer";
+let myBalance = 10000; // Dollar
+let myPin = 1234;
+let pinAnswer = await inquirer.prompt(
+[
+    {
+      name: "pin",
+      message: "enter your pin",
+      type: "number"
+   }
+]
+);
+if(pinAnswer.pin === myPin) {
+     console.log("Correct pin code!!!")
+let operationAns = await inquirer.prompt(
+    [
+      {
+        name: "operation",
+        message: "Please select option",
+        type: "list",
+        choices :["withdraw", "check balance"]      
+        }
+    ]
+)
+        if(operationAns.operation === "withdraw") {
+            let amountAns = await inquirer.prompt(
+                [
+                    {
+                      name: "amount",
+                      message: "enter your amount",
+                      type: "number"
+                    }
+                ]
+                );
+                myBalance -= amountAns.amount;
+                console.log("your remaining balance is: " + myBalance);      
+        }
+    }
+else {
+    console.log("Incorrect pin number");
+}
+
+
+
